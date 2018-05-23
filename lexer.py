61504f5a692bc26f19ed9afecf4e84bff8a9ba63
JakubGrobelny <300481@uwr.edu.pl>
2018-05-24 00:38:13 +0200

    Fixed lexer

--- lexer.py
+++ lexer.py
@@ -1,8 +1,26 @@
+def split_line(line : str):
+    token = ""
+    tokens = []
+    for c in line:
+        if c.isspace() or c == ',':
+            if token != "":
+                tokens.append(token)
+                token = ""
+        else:
+            token += c
+    if token != "":
+        tokens.append(token)
+    return tokens
+
 def tokenize(lines : list):
     # Removing comments
     lines = map(lambda line : line.split('#')[0], lines)
+    # Splitting by whitespace and commas
+    lines = map(split_line, lines)
+    # Removing empty lines
+    lines = list(filter(None, lines))
 
     for line in lines:
         print(line)
-        
+
     return lines
