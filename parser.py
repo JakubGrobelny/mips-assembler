f4d7ecd98e0de2d3b1f8613394e2c253ebcfc4aa
JakubGrobelny <300481@uwr.edu.pl>
2018-06-01 00:57:14 +0200

    Minor changes

--- parser.py
+++ parser.py
@@ -6,7 +6,7 @@ def parse(tokens : list):
             data = instructions[tokens[0]]
             type = data[0][0]
             pattern = data[1:]
-            if not check_pattern(pattern, token):
+            if not does_match_pattern(pattern, token):
                 raise Exception("Error! Invalid instruction operands! " + token[0]
                                 + ": expected " + ', '.join(pattern) + ", given "
                                 + ', '.join(token[1:]))
@@ -17,26 +17,42 @@ def parse(tokens : list):
                 opcode = data[0][1]
                 funct  = '0' * 6
             else:
-                raise Exception("Assembler error! Invalid instruction type!")
+                raise Exception("Assembler error! Invalid instruction type: " + type)
         else:
             raise Exception("Error! Invalid instruction " + tokens[0])
 
-def  
-
-symbolic_registers = [
+internal_symbolic_registers = [
     's',
     'd',
     't'
 ]
 
-def is_register(token : str):
+def is_decimal(number : str) -> bool:
     
-def check_pattern(pattern : list, instruction : list):
+    
+def is_hexadecimal(number : str) -> bool:
+    
+
+def match_number(pattern : str, number : str) -> bool:
+    type = pattern[0]
+    size = int(pattern[1:], 10)
+    max_size = 2**size - 1
+    if pattern[0] == 'i':
+        # TODO: map parse_number to token list before checking pattern
+    else:
+        raise Exception("Assembler error! Invalid constant type: " + pattern[0])
+
+def is_register(token : str) -> bool:
+    
+    
+def does_match_pattern(pattern : list, instruction : list) -> bool:
     if len(pattern) != len(instruction[1:]):
         return False
     for operand in zip(pattern, instruction[1:]):
         correct = operand[0]
         given   = operand[1]
-        if correct in symbolic_registers:
+        # Operand can be either a register or a numeric constant
+        if correct in internal_symbolic_registers:
             return is_register(given)
-        elif 
+        else:
+            return match_number(correct, given)
---
+++ util.py
@@ -0,0 +1,2 @@
+def bin_to_hex(bin : str) -> str:
+    return hex(int(bin, 2))[2:]
