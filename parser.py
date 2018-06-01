32656aee7a6da1379b2b8a169de5fc5b30683fe9
JakubGrobelny <300481@uwr.edu.pl>
2018-06-01 23:43:44 +0200

    Further parser development

--- parser.py
+++ parser.py
@@ -1,58 +1,154 @@
 from instructions import *
 
-def parse(tokens : list):
-    for token in tokens:
-        if token[0] in instructions:
-            data = instructions[tokens[0]]
-            type = data[0][0]
-            pattern = data[1:]
-            if not does_match_pattern(pattern, token):
-                raise Exception("Error! Invalid instruction operands! " + token[0]
-                                + ": expected " + ', '.join(pattern) + ", given "
-                                + ', '.join(token[1:]))
-            if type == 'R':
-                opcode = '0' * 6
-                funct  = data[0][1]
-            elif type == 'I':
-                opcode = data[0][1]
-                funct  = '0' * 6
-            else:
-                raise Exception("Assembler error! Invalid instruction type: " + type)
+
+# Parses a single line of assembly program.
+def parse(tokens: list):
+    if tokens[0] in instructions:
+        data = instructions[tokens[0]]
+        type = data[0][0]
+        pattern = data[1:]
+
+        # Parsing integer literals
+        tokens = list(map(try_parse_number, tokens))
+
+        # Removing symbolic register names
+        tokens = list(map(try_translate_symbolic_register, tokens))
+
+        if not does_match_pattern(pattern, tokens):
+            raise Exception("Error! Invalid instruction operands! " + tokens[0]
+                            + ": expected " + ', '.join(pattern) + ", given "
+                            + ', '.join(tokens[1:]))
+
+        if type == 'R':
+            opcode = '0' * 6
+            s_reg  = '0' * 5
+            t_reg  = '0' * 5
+            d_reg  = '0' * 5
+            shamt  = '0' * 5
+            funct  = data[0][1]
+
+        elif type == 'I':
+            opcode = data[0][1]
+            
         else:
-            raise Exception("Error! Invalid instruction " + tokens[0])
+            raise Exception("Assembler error! Invalid instruction type: " + type)
+    else:
+        raise Exception("Error! Invalid instruction " + tokens[0])
 
-internal_symbolic_registers = [
-    's',
-    'd',
-    't'
-]
 
-def is_decimal(number : str) -> bool:
-    
-    
-def is_hexadecimal(number : str) -> bool:
-    
+def try_parse_number(number: str):
+    try:
+        return parse_integer(number)
+    except:
+        return number
 
-def match_number(pattern : str, number : str) -> bool:
+
+def parse_integer(number: str) -> int:
+    if is_decimal(number):
+        return int(number, 10)
+    elif is_hexadecimal(number):
+        return int(number, 16)
+    else:
+        raise Exception("Error! Invalid integer literal " + number)
+
+
+def is_decimal(number: str) -> bool:
+    try:
+        int(number, 10)
+        return True
+    except:
+        return False
+
+
+def is_hexadecimal(number: str) -> bool:
+    if number[:1] != '0x':
+        return False
+    try:
+        int(number, 16)
+        return True
+    except:
+        return False
+
+
+def does_number_match(pattern: str, number) -> bool:
     type = pattern[0]
+    # Extracting the desired size of the constant
     size = int(pattern[1:], 10)
-    max_size = 2**size - 1
+
+    # Finding the max and min number that can be represented with given number of bits.
+    # This only works with signed integers for now:
+    max_size = 2 ** size - 1
+    min_size = -(2 ** size)
+
     if pattern[0] == 'i':
-        # TODO: map parse_number to token list before checking pattern
+        if number is int and max_size >= number >= min_size:
+            return True
+        else:
+            return False
     else:
         raise Exception("Assembler error! Invalid constant type: " + pattern[0])
 
-def is_register(token : str) -> bool:
-    
-    
-def does_match_pattern(pattern : list, instruction : list) -> bool:
+
+internal_symbolic_registers = ['s', 'd', 't']
+
+
+def try_translate_symbolic_register(register):
+    if register is str and register[0] == '$':
+        if is_decimal(register[1:]) and 0 <= int(register[1:]) < 32:
+            return register
+        else:
+            if register[1:] == "zero":
+                return "$0"
+            elif register[1:] == "at":
+                return "$1"
+            elif register[1:] == "gp":
+                return "$28"
+            elif register[1:] == "sp":
+                return "$29"
+            elif register[1:] == "fp":
+                return "$30"
+            elif register[1:] == "ra":
+                return "$31"
+            elif is_decimal(register[2:]):
+                index = int(register[2:])
+                name  = register[1]
+                if name == 'v' and 0 <= index <= 1:
+                    return "$" + str(2 + index)
+                if name == 'a' and 0 <= index <= 3:
+                    return "$" + str(4 + index)
+                if name == 't' and 0 <= index <= 9:
+                    if index <= 7:
+                        return '$' + str(8 + index)
+                    else:
+                        return '$' + str(24 + index)
+                if name == 's' and 0 <= index <= 7:
+                    return '$' + str(16 + index)
+                if name == 'k' and 0 <= index <= 1:
+                    return '$' + str(26 + index)
+                else:
+                    return register
+            else:
+                return register
+    else:
+        return register
+
+
+def is_register(token: str) -> bool:
+    if len(token) < 2 or token[0] != '$':
+        return False
+    elif 0 <= int(token[1:]) < 32:
+        return True
+    return False
+
+
+def does_match_pattern(pattern: list, instruction: list) -> bool:
     if len(pattern) != len(instruction[1:]):
         return False
     for operand in zip(pattern, instruction[1:]):
         correct = operand[0]
-        given   = operand[1]
+        given = operand[1]
         # Operand can be either a register or a numeric constant
         if correct in internal_symbolic_registers:
             return is_register(given)
         else:
-            return match_number(correct, given)
+            return does_number_match(correct, given)
